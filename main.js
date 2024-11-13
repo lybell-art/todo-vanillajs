53f93e1edad4d401741e5900d9d606426477de5a
lybell <futireflugel@gmail.com>
2024-11-13 21:21:44 +0900

    [Feat] todo 입력 폼 추가

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello, world!");
+const $todoInsertForm = document.getElementById("todo-insert");
+const $todoList = document.querySelector("todo-list");
+const $todoInput = $todoInsertForm.querySelector("input");
+
+$todoInsertForm.addEventListener("submit", (e)=>{
+	e.preventDefault();
+	if(!$todoInput.value) return;
+
+	console.log($todoInput.value);
+	$todoInput.value = "";
+});
